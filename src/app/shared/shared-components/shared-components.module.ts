c84ae05731f2fa4ed1d7ba9c5217c4898f8dd54a
oliveiravitor32 <oliveiravitor327@gmail.com>
2025-03-01 13:59:22 -0300

    feat(header): finalize structure, styles, and responsiveness

--- src/app/shared/shared-components/shared-components.module.ts
+++ src/app/shared/shared-components/shared-components.module.ts
@@ -1,13 +1,23 @@
-import { NavbarModule } from './../../components/navbar/navbar.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
-import { FooterModule } from '../../components/footer/footer.module';
 import { BannerComponent } from '../../components/banner/banner.component';
+
+import { NavbarModule } from './../../components/navbar/navbar.module';
+import { FooterModule } from '../../components/footer/footer.module';
+
+import { NgOptimizedImage } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [HeaderComponent, BannerComponent],
-  imports: [CommonModule, NavbarModule, FooterModule],
+  imports: [
+    CommonModule,
+    NavbarModule,
+    FooterModule,
+    NgOptimizedImage,
+    FontAwesomeModule,
+  ],
   exports: [HeaderComponent],
 })
 export class SharedComponentsModule {}
