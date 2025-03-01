5e6c4a131ed5c3109f5f9605aec7b398964f8cd3
oliveiravitor32 <oliveiravitor327@gmail.com>
2025-03-01 15:25:31 -0300

    style: add custom colors to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          extralight: "#abc4ed",
+          light: "#4F74C8",
+          default: "#4065BA",
+          dark: "#2E51A2",
+        },
+        gray: {
+          extralight: "#333333",
+          light: "#222222",
+          default: "#181818",
+          dark: "#121212",
+        },
+      },
+    },
   },
   plugins: [],
 };
