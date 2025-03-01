6ed3ea204791e86a2bb0f2a730e99b5992597ba2
oliveiravitor32 <oliveiravitor327@gmail.com>
2025-03-01 13:57:21 -0300

    build: add @fortawesome/angular-fontawesome for icon support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedComponentsModule } from './shared/shared-components/shared-components.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedComponentsModule],
+  imports: [BrowserModule, AppRoutingModule, SharedComponentsModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
