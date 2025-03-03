04159b667b5d76d65cc5a2143369a3de17bac652
oliveiravitor32 <oliveiravitor327@gmail.com>
2025-03-02 21:42:27 -0300

    feat(app): import HttpClientModule in AppModule to enable HTTP requests
    - Imported HttpClientModule in AppModule to use HttpClient for making HTTP requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedComponentsModule } from './shared/shared-components/shared-components.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedComponentsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedComponentsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
