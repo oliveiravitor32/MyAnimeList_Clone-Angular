0c9d94b1f83923d4a74650895423a42fc9dee02a
oliveiravitor32 <oliveiravitor327@gmail.com>
2025-03-04 19:05:43 -0300

    feat(navbar): improve usability by clearing search results on input clear and implement unsubscribe on destroy
    - Implemented logic to remove the last request result when the user clears the search bar for better usability
    - Implemented the unsubscribe function when the component is destroyed to avoid memory leaks

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
 import { HttpParams } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import {
   debounceTime,
   distinctUntilChanged,
   filter,
+  pairwise,
   switchMap,
   take,
 } from 'rxjs/operators';
@@ -19,8 +21,10 @@ import { CategoryTypeEnum } from './../../enums/categoy-type.enum';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
   searchedData: AnimesResponseDataList = [];
+
+  searchSubscription: Subscription | null | undefined = null;
 
   searchForm = new FormGroup({
     category: new FormControl(CategoryTypeEnum.ALL),
@@ -42,13 +46,26 @@ export class NavbarComponent implements OnInit {
       this._searchService
     );
 
-    this.searchForm
+    this.searchSubscription = this.searchForm
       .get('text')
       ?.valueChanges.pipe(
-        debounceTime(400), // Wait for 400ms pause in events
+        debounceTime(300), // Wait for 400ms pause in events
         distinctUntilChanged(), // Only emit if value is different from previous value
-        filter((query) => query !== null && query.trim().length > 0), // Filter out empty values
-        switchMap((query) => {
+        pairwise(), // Emit the previous and current values as a tuple
+        filter(([prevQuery, currQuery]) => {
+          // Clears last request cache when user clears search bar
+          if (
+            currQuery?.length === 0 &&
+            prevQuery?.length &&
+            prevQuery.length > 0
+          ) {
+            console.log('Clearing search data');
+            this.searchedData = [];
+          }
+
+          return currQuery !== null && currQuery.trim().length > 0;
+        }), // Filter out empty values
+        switchMap(([prevQuery, currQuery]) => {
           const category = this.searchForm.get('category')
             ?.value as CategoryTypeEnum;
 
@@ -58,16 +75,24 @@ export class NavbarComponent implements OnInit {
             .set('limit', '10');
 
           const searchMethod = categoryMethodMap[category];
-          return searchMethod(query!, additionalParams).pipe(take(1));
+          return searchMethod(currQuery!, additionalParams).pipe(take(1));
         }) // Cancel previous request if new request is made and take only the first response
       )
       .subscribe((results) => {
         this.searchedData = results.data;
         console.log(results.data);
       });
+    this.searchForm.get('text')?.setValue('hunter');
   }
 
   onFormSubmit() {
     // TODO: Implement the navigation to the search page
+  }
+
+  ngOnDestroy(): void {
+    // Unsubscribe from the search subscription to avoid memory leaks
+    if (this.searchSubscription) {
+      this.searchSubscription.unsubscribe();
+    }
   }
 }
