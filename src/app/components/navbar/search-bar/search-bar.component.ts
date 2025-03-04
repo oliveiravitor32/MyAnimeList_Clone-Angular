0efb7b3289bcde34609b6a93899b0608974836b6
oliveiravitor32 <oliveiravitor327@gmail.com>
2025-03-04 00:23:25 -0300

    feat(search-bar): implement dynamic show/hide of results with ViewChild
    - Implemented ViewChild with EventListener to show and hide results dynamically based on input focus and text size

--- src/app/components/navbar/search-bar/search-bar.component.ts
+++ src/app/components/navbar/search-bar/search-bar.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { faMagnifyingGlass, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { AnimesResponseDataList } from '../../../types/animes-response-data-list';
@@ -9,14 +17,18 @@ import { categoryTypeArray } from '../../../utils/category-type-description-map'
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css',
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements AfterViewInit {
   @Input({ required: true }) searchForm!: FormGroup;
   @Input({ required: true }) searchedData: AnimesResponseDataList = [];
 
   @Output('onFormSubmit') onFormSubmitEmitt = new EventEmitter<void>();
 
+  @ViewChild('searchTextInput') searchInput!: ElementRef<HTMLInputElement>;
+
   searchIcon = faMagnifyingGlass;
   closeIcon = faTimes;
+
+  showResults = false;
 
   get categoryTypeArray() {
     return categoryTypeArray;
@@ -24,6 +36,20 @@ export class SearchBarComponent {
 
   get searchedText() {
     return this.searchForm.get('text')?.value;
+  }
+
+  ngAfterViewInit() {
+    this.watchInputTextFocusAndShowResults();
+  }
+
+  watchInputTextFocusAndShowResults() {
+    this.searchInput.nativeElement.addEventListener('focus', () => {
+      this.showResults = true;
+    });
+
+    this.searchInput.nativeElement.addEventListener('blur', () => {
+      this.showResults = false;
+    });
   }
 
   clearSearch() {
